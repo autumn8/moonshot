9ab0ba85a3852a61bbfd3c807d9c8db14d5e269b
Brad <brad.roodt@gmail.com>
2025-02-26 16:47:18 +0200

    Adds hire screen info

--- client/app/(tabs)/wait.tsx
+++ client/app/(tabs)/wait.tsx
@@ -1,5 +1,30 @@
-import { Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function WaitScreen() {
-  return <Text>Nothing to see here</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Hire This Guy!</Text>
+      <Image
+        source={require('../../assets/images/hire-this-guy.png')}
+        style={styles.image}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 40,
+    marginBottom: 30,
+  },
+  image: {
+    height: 200,
+    width: 200,
+  },
+  info: {},
+});
